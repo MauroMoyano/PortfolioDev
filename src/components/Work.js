c7a0de9cc62559271bb47ce90490a52db7c9ed0d
MauroMoyano <mauromoyanodev@gmail.com>
2023-05-26 04:50:31 -0300

    work on secction my work

--- src/components/Work.js
+++ src/components/Work.js
@@ -4,8 +4,8 @@ import { motion } from "framer-motion";
 // variants
 import { fadeIn } from "../variants";
 // img
-import img1 from "../assets/portfolio-img1.png";
-import img2 from "../assets/portfolio-img2.png";
+import img1 from "../assets/portfolio-img1.jpg";
+import img2 from "../assets/portfolio-img2.jpg";
 import img3 from "../assets/portfolio-img3.png";
 
 const Work = () => {
@@ -13,70 +13,81 @@ const Work = () => {
     <section className="section" id="work">
       <div className="container mx-auto">
         <div className="flex flex-col lg:flex-row gap-x-10">
-          <motion.div 
-          variants={fadeIn("right", 0.2)}
-          initial="hidden"
-          whileInView={"show"}
-          viewport={{ once: false, amount: 0.3 }}
-          className="flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0">
+          <motion.div
+            variants={fadeIn("right", 0.2)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0"
+          >
             {/* text */}
             <div>
               <h2 className="h2 leading-tight text-accent">
                 My Lastest <br />
                 Work.
               </h2>
-              <p className="max-w-sm mb-16">
-                Lorem ipsunLorem dolorenqueLorem ipsunLorem dolorenqueLoremipsunLorem dolorenqueLoremipsunLorem 
+              <p className="max-w-[550px] mb-16">
+                Browse through my portfolio of web pages, showcasing examples of
+                my web design and development skills. Discover modern designs,
+                intuitive interfaces, and creative solutions I have implemented
+                to deliver memorable online experiences.
               </p>
-              <button className="btn btn-sm">View all projects</button>
+              <button className="btn btn-sm ">View all projects</button>
             </div>
             {/* image */}
-            <div className="group relative overflow-hidden border-2 border-white/50 rounded-xl">
-              {/* overlay */}
-              <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
-              {/* img */}
-              <img
-                className="group-hover:scale-125 transition-all duration-500"
-                src={img1}
-                alt=""
-              />
-              {/* pretitle */}
-              <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
-                <span className="text-gradient">UI/UX Design</span>
+            <a href="https://emprendar-front.vercel.app/" target="_blank">
+              <div className="group relative overflow-hidden border-2 border-white/50 rounded-xl">
+                {/* overlay */}
+                <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
+                {/* img */}
+                <img
+                  className="group-hover:scale-125 transition-all duration-500"
+                  src={img1}
+                  alt=""
+                />
+                {/* pretitle */}
+                <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
+                  <span className="text-gradient">
+                    Full Stack participation
+                  </span>
+                </div>
+                {/* title */}
+                <div className="absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50">
+                  <span className="text-3xl text-white">Emprendar</span>
+                </div>
               </div>
-              {/* title */}
-              <div className="absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50">
-                <span className="text-3xl text-white">Project title</span>
-              </div>
-            </div>
+            </a>
           </motion.div>
-          <motion.div 
-          variants={fadeIn("left", 0.2)}
-          initial="hidden"
-          whileInView={"show"}
-          viewport={{ once: false, amount: 0.3 }}
-          className="flex-1 flex flex-col gap-y-10 justify-between">
+          <motion.div
+            variants={fadeIn("left", 0.2)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1 flex flex-col gap-y-10 justify-between"
+          >
             {/* Imagen 2*/}
-            <div className="group relative overflow-hidden border-2 border-white/50 rounded-xl">
-              {/* overlay */}
-              <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
-              {/* img */}
-              <img
-                className="group-hover:scale-125 transition-all duration-500"
-                src={img2}
-                alt=""
-              />
-              {/* pretitle */}
-              <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
-                <span className="text-gradient">UI/UX Design</span>
+            <a href="https://pi-food-inky.vercel.app/" target="_blank">
+              <div className="group relative overflow-hidden border-2 border-white/50 rounded-xl">
+                {/* overlay */}
+                <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
+                {/* img */}
+                <img
+                  className="group-hover:scale-125 transition-all duration-500"
+                  src={img2}
+                  alt=""
+                />
+                {/* pretitle */}
+                <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
+                  <span className="text-gradient">Full Stack PERN</span>
+                </div>
+                {/* title */}
+                <div className="absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50">
+                  <span className="text-3xl text-white">Foods</span>
+                </div>
               </div>
-              {/* title */}
-              <div className="absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50">
-                <span className="text-3xl text-white">Project title</span>
-              </div>
-            </div>
-              <div>
-                {/* imagen 3 */}
+            </a>
+            <div>
+              {/* imagen 3 */}
               <div className="group relative overflow-hidden border-2 border-white/50 rounded-xl">
                 {/* overlay */}
                 <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
